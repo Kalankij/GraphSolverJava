bc455eb79b6461b4d31c92126197ce6bc8e2698e
Bartek <01169564@pw.edu.pl>
2022-06-03 20:44:41 +0200

    Poprawione rysowanie dla grafu niespójnego

--- GrafSolver/src/main/java/grafsolver/Draw.java
+++ GrafSolver/src/main/java/grafsolver/Draw.java
@@ -29,65 +29,87 @@ public class Draw {
         int k = 0;
         for (Wierzcholek w : graf) {
             if (i != (width * (j + 1)) - 1) { //prawa
-                if (w.krawedzi.size() > 0) {
+                if (w.krawedzi.size() > 0 && w.getNumer() == w.krawedzi.get(0).getTo() - 1) {
                     if (width == length)
                         line = new Line(700 / width * k + 0.75 * 700 / width, 60 + 700 / length * j + 0.25 * 700 / length, 700 / width * (k + 1), 60 + 700 / length * j + 0.25 * 700 / length);
-                    //if(w.getNumer() == w.krawedzi.get(0).getTo() - 1 && w.krawedzi.get(0).getWaga() > 0.5)
                     if (width > length)
                         line = new Line(700 / width * k + 0.75 * 700 / width, 60 + 700 / width * j + 0.25 * 700 / width, 700 / width * (k + 1), 60 + 700 / width * j + 0.25 * 700 / width);
                     if (width < length)
                         line = new Line(700 / length * k + 0.75 * 700 / length, 60 + 700 / length * j + 0.25 * 700 / length, 700 / length * (k + 1), 60 + 700 / length * j + 0.25 * 700 / length);
-                    if (w.getNumer() == w.krawedzi.get(0).getTo() - 1) {
-                        if (w.krawedzi.get(0).getWaga() <= red)
-                            line.setStroke(Color.RED);
-                        if (w.krawedzi.get(0).getWaga() > red && w.krawedzi.get(0).getWaga() <= orange)
-                            line.setStroke(Color.ORANGE);
-                        if (w.krawedzi.get(0).getWaga() > orange && w.krawedzi.get(0).getWaga() <= yellow)
-                            line.setStroke(Color.YELLOW);
-                        if (w.krawedzi.get(0).getWaga() > yellow && w.krawedzi.get(0).getWaga() <= lightgreena)
-                            line.setStroke(Color.LIGHTGREEN);
-                        if (w.krawedzi.get(0).getWaga() > lightgreena && w.krawedzi.get(0).getWaga() <= limegreen)
-                            line.setStroke(Color.LIMEGREEN);
-                        if (w.krawedzi.get(0).getWaga() > limegreen && w.krawedzi.get(0).getWaga() <= lightgreenb)
-                            line.setStroke(Color.LIGHTGREEN);
-                        if (w.krawedzi.get(0).getWaga() > lightgreenb && w.krawedzi.get(0).getWaga() <= cyan)
-                            line.setStroke(Color.CYAN);
-                        if (w.krawedzi.get(0).getWaga() > cyan && w.krawedzi.get(0).getWaga() <= lightblue)
-                            line.setStroke(Color.LIGHTBLUE);
-                        if (w.krawedzi.get(0).getWaga() > lightblue)
-                            line.setStroke(Color.BLUE);
-                    }
+                    if (w.krawedzi.get(0).getWaga() <= red)
+                        line.setStroke(Color.RED);
+                    if (w.krawedzi.get(0).getWaga() > red && w.krawedzi.get(0).getWaga() <= orange)
+                        line.setStroke(Color.ORANGE);
+                    if (w.krawedzi.get(0).getWaga() > orange && w.krawedzi.get(0).getWaga() <= yellow)
+                        line.setStroke(Color.YELLOW);
+                    if (w.krawedzi.get(0).getWaga() > yellow && w.krawedzi.get(0).getWaga() <= lightgreena)
+                        line.setStroke(Color.LIGHTGREEN);
+                    if (w.krawedzi.get(0).getWaga() > lightgreena && w.krawedzi.get(0).getWaga() <= limegreen)
+                        line.setStroke(Color.LIMEGREEN);
+                    if (w.krawedzi.get(0).getWaga() > limegreen && w.krawedzi.get(0).getWaga() <= lightgreenb)
+                        line.setStroke(Color.LIGHTGREEN);
+                    if (w.krawedzi.get(0).getWaga() > lightgreenb && w.krawedzi.get(0).getWaga() <= cyan)
+                        line.setStroke(Color.CYAN);
+                    if (w.krawedzi.get(0).getWaga() > cyan && w.krawedzi.get(0).getWaga() <= lightblue)
+                        line.setStroke(Color.LIGHTBLUE);
+                    if (w.krawedzi.get(0).getWaga() > lightblue)
+                        line.setStroke(Color.BLUE);
                     root.getChildren().add(line);
                 }
             }
             if (i < width * length - width) { //dół
-                if (w.krawedzi.size() > 1) {
+                if (w.krawedzi.size() > 1 && w.getNumer() == w.krawedzi.get(0).getTo() - 1 && w.getNumer() == w.krawedzi.get(1).getTo() - width) {
                     if (width == length)
                         line = new Line(700 / width * k + 0.5 * 700 / width, 60 + 700 / length * j + 0.75 * 700 / length, 700 / width * k + 0.5 * 700 / width, 60 + 700 / length * (j + 1));
                     if (width > length)
                         line = new Line(700 / width * k + 0.5 * 700 / width, 60 + 700 / width * j + 0.75 * 700 / width, 700 / width * k + 0.5 * 700 / width, 60 + 700 / width * (j + 1));
                     if (width < length)
                         line = new Line(700 / length * k + 0.5 * 700 / length, 60 + 700 / length * j + 0.75 * 700 / length, 700 / length * k + 0.5 * 700 / length, 60 + 700 / length * (j + 1));
-                    if (w.getNumer() == w.krawedzi.get(1).getTo() - width || w.getNumer() == w.krawedzi.get(0).getTo() - width) {
                         if (w.krawedzi.get(1).getWaga() <= red)
                             line.setStroke(Color.RED);
-                        if (w.krawedzi.get(1).getWaga() > red && w.krawedzi.get(0).getWaga() <= orange)
+                        if (w.krawedzi.get(1).getWaga() > red && w.krawedzi.get(1).getWaga() <= orange)
                             line.setStroke(Color.ORANGE);
-                        if (w.krawedzi.get(1).getWaga() > orange && w.krawedzi.get(0).getWaga() <= yellow)
+                        if (w.krawedzi.get(1).getWaga() > orange && w.krawedzi.get(1).getWaga() <= yellow)
                             line.setStroke(Color.YELLOW);
-                        if (w.krawedzi.get(1).getWaga() > yellow && w.krawedzi.get(0).getWaga() <= lightgreena)
+                        if (w.krawedzi.get(1).getWaga() > yellow && w.krawedzi.get(1).getWaga() <= lightgreena)
                             line.setStroke(Color.LIGHTGREEN);
-                        if (w.krawedzi.get(1).getWaga() > lightgreena && w.krawedzi.get(0).getWaga() <= limegreen)
+                        if (w.krawedzi.get(1).getWaga() > lightgreena && w.krawedzi.get(1).getWaga() <= limegreen)
                             line.setStroke(Color.LIMEGREEN);
-                        if (w.krawedzi.get(1).getWaga() > limegreen && w.krawedzi.get(0).getWaga() <= lightgreenb)
+                        if (w.krawedzi.get(1).getWaga() > limegreen && w.krawedzi.get(1).getWaga() <= lightgreenb)
                             line.setStroke(Color.LIGHTGREEN);
-                        if (w.krawedzi.get(1).getWaga() > lightgreenb && w.krawedzi.get(0).getWaga() <= cyan)
+                        if (w.krawedzi.get(1).getWaga() > lightgreenb && w.krawedzi.get(1).getWaga() <= cyan)
                             line.setStroke(Color.CYAN);
-                        if (w.krawedzi.get(1).getWaga() > cyan && w.krawedzi.get(0).getWaga() <= lightblue)
+                        if (w.krawedzi.get(1).getWaga() > cyan && w.krawedzi.get(1).getWaga() <= lightblue)
                             line.setStroke(Color.LIGHTBLUE);
                         if (w.krawedzi.get(1).getWaga() > lightblue)
                             line.setStroke(Color.BLUE);
-                    }
+                    root.getChildren().add(line);
+                }
+                if (w.krawedzi.size() > 0 && w.getNumer() == w.krawedzi.get(0).getTo() - width) {
+                    if (width == length)
+                        line = new Line(700 / width * k + 0.5 * 700 / width, 60 + 700 / length * j + 0.75 * 700 / length, 700 / width * k + 0.5 * 700 / width, 60 + 700 / length * (j + 1));
+                    if (width > length)
+                        line = new Line(700 / width * k + 0.5 * 700 / width, 60 + 700 / width * j + 0.75 * 700 / width, 700 / width * k + 0.5 * 700 / width, 60 + 700 / width * (j + 1));
+                    if (width < length)
+                        line = new Line(700 / length * k + 0.5 * 700 / length, 60 + 700 / length * j + 0.75 * 700 / length, 700 / length * k + 0.5 * 700 / length, 60 + 700 / length * (j + 1));
+                    if (w.krawedzi.get(0).getWaga() <= red)
+                        line.setStroke(Color.RED);
+                    if (w.krawedzi.get(0).getWaga() > red && w.krawedzi.get(0).getWaga() <= orange)
+                        line.setStroke(Color.ORANGE);
+                    if (w.krawedzi.get(0).getWaga() > orange && w.krawedzi.get(0).getWaga() <= yellow)
+                        line.setStroke(Color.YELLOW);
+                    if (w.krawedzi.get(0).getWaga() > yellow && w.krawedzi.get(0).getWaga() <= lightgreena)
+                        line.setStroke(Color.LIGHTGREEN);
+                    if (w.krawedzi.get(0).getWaga() > lightgreena && w.krawedzi.get(0).getWaga() <= limegreen)
+                        line.setStroke(Color.LIMEGREEN);
+                    if (w.krawedzi.get(0).getWaga() > limegreen && w.krawedzi.get(0).getWaga() <= lightgreenb)
+                        line.setStroke(Color.LIGHTGREEN);
+                    if (w.krawedzi.get(0).getWaga() > lightgreenb && w.krawedzi.get(0).getWaga() <= cyan)
+                        line.setStroke(Color.CYAN);
+                    if (w.krawedzi.get(0).getWaga() > cyan && w.krawedzi.get(0).getWaga() <= lightblue)
+                        line.setStroke(Color.LIGHTBLUE);
+                    if (w.krawedzi.get(0).getWaga() > lightblue)
+                        line.setStroke(Color.BLUE);
                     root.getChildren().add(line);
                 }
             }
@@ -122,32 +144,34 @@ public class Draw {
                     }
                 }
                 if (j != 0) {
-                    if (w.krawedzi.size() > 1 && w.getNumer() == w.krawedzi.get(w.krawedzi.size() - 2).getTo() + 1) {
-                        if (width == length)
-                            line = new Line(700 / width * k, 60 + 700 / length * j + 0.5 * 700 / length, 700 / width * (k - 1) + 0.75 * 700 / width, 60 + 700 / length * j + 0.5 * 700 / length);
-                        if (width > length)
-                            line = new Line(700 / width * k, 60 + 700 / width * j + 0.5 * 700 / width, 700 / width * (k - 1) + 0.75 * 700 / width, 60 + 700 / width * j + 0.5 * 700 / width);
-                        if (width < length)
-                            line = new Line(700 / length * k, 60 + 700 / length * j + 0.5 * 700 / length, 700 / length * (k - 1) + 0.75 * 700 / length, 60 + 700 / length * j + 0.5 * 700 / length);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= red)
-                            line.setStroke(Color.RED);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > red && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= orange)
-                            line.setStroke(Color.ORANGE);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > orange && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= yellow)
-                            line.setStroke(Color.YELLOW);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > yellow && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= lightgreena)
-                            line.setStroke(Color.LIGHTGREEN);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > lightgreena && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= limegreen)
-                            line.setStroke(Color.LIMEGREEN);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > limegreen && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= lightgreenb)
-                            line.setStroke(Color.LIGHTGREEN);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > lightgreenb && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= cyan)
-                            line.setStroke(Color.CYAN);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > cyan && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= lightblue)
-                            line.setStroke(Color.LIGHTBLUE);
-                        if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > lightblue)
-                            line.setStroke(Color.BLUE);
-                        root.getChildren().add(line);
+                    if (w.krawedzi.size() > 0 && w.getNumer() == w.krawedzi.get(w.krawedzi.size() - 1).getTo() + width) {
+                        if (w.krawedzi.size() > 1 && w.getNumer() == w.krawedzi.get(w.krawedzi.size() - 2).getTo() + 1) {
+                            if (width == length)
+                                line = new Line(700 / width * k, 60 + 700 / length * j + 0.5 * 700 / length, 700 / width * (k - 1) + 0.75 * 700 / width, 60 + 700 / length * j + 0.5 * 700 / length);
+                            if (width > length)
+                                line = new Line(700 / width * k, 60 + 700 / width * j + 0.5 * 700 / width, 700 / width * (k - 1) + 0.75 * 700 / width, 60 + 700 / width * j + 0.5 * 700 / width);
+                            if (width < length)
+                                line = new Line(700 / length * k, 60 + 700 / length * j + 0.5 * 700 / length, 700 / length * (k - 1) + 0.75 * 700 / length, 60 + 700 / length * j + 0.5 * 700 / length);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= red)
+                                line.setStroke(Color.RED);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > red && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= orange)
+                                line.setStroke(Color.ORANGE);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > orange && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= yellow)
+                                line.setStroke(Color.YELLOW);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > yellow && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= lightgreena)
+                                line.setStroke(Color.LIGHTGREEN);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > lightgreena && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= limegreen)
+                                line.setStroke(Color.LIMEGREEN);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > limegreen && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= lightgreenb)
+                                line.setStroke(Color.LIGHTGREEN);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > lightgreenb && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= cyan)
+                                line.setStroke(Color.CYAN);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > cyan && w.krawedzi.get(w.krawedzi.size() - 2).getWaga() <= lightblue)
+                                line.setStroke(Color.LIGHTBLUE);
+                            if (w.krawedzi.get(w.krawedzi.size() - 2).getWaga() > lightblue)
+                                line.setStroke(Color.BLUE);
+                            root.getChildren().add(line);
+                        }
                     }
                 }
             }
